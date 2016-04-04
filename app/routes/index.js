b96cfc7cc289803ec366f9c6106743c0bb6eeeee
五月的风 <578595193@qq.com>
2016-04-04 12:31:14 +0800

    get Gold Price tmp

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,4 +13,30 @@ router.get('/', function(req, res, next) {
   });
 });
 
+
+router.get('/goldPrice', function(request, response, next){
+  var http = require('https');
+  var options = {
+    hostname: 'www.g-banker.com',
+    path: '/info/price',
+    method: 'POST',
+    secure: true,
+    headers: {
+      'Connection' : 'keep-alive',
+      'Content-Type': 'application/json;charset=UTF-8',
+      'aw-tenant-code': 'q6T2GUtlXf/oOaiFwfiMFYWtMKveaH+19BCrxzRQvoc='
+    }
+  };
+  var req = http.request(options, function (res) {
+    var data = '';
+    res.on('data', function (chunk) {
+      data += chunk;
+    });
+    res.on('end', function(){
+      response.json(JSON.parse(data));
+    });
+  });
+  req.end();
+});
+
 module.exports = router;
