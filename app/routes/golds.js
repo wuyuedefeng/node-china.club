2a286b9e57a3ad65ff1c9a7e9cce16b15ae3139d
王森 <wangs@tongxinyiliao.com>
2016-05-13 18:03:14 +0800

    .

--- app/routes/golds.js
+++ app/routes/golds.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+
+    if (res.locals.user.name != 'wuyuedefeng'){
+        res.send('权限不足');
+    }
+
     res.render('golds/index', {
         title: '金价获取'
     });
